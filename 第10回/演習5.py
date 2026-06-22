82453a2ab2192f0f081b788abfda59812f2c19be
aiking-01 <aiki03023@gmail.com>
2026-06-22 16:07:11 +0900

    演習5を実装

---
+++ 第10回/演習5.py
@@ -0,0 +1,50 @@
+#---1---
+import random
+def dice5():
+    randlist = []
+    for i in range(5):
+        randlist.append(random.randint(1, 6))
+    return randlist
+
+def judge0(l):
+    total = sum(l)
+    if total >= 20:
+        return '吉'
+    else:
+        return '凶'
+
+print(dice5())
+print(judge0([10, 11]))
+print(judge0([10, 9]))
+print(judge0(dice5()))
+
+
+
+#---2---
+def judge1(l):
+    count = [0] * 7
+    for i in l:
+        count[i] += 1
+    max_count = max(count)
+    if max_count >= 3:
+        return '大吉'
+    elif max_count == 2:
+        return '吉'
+    else:
+        return ''
+
+print(judge1([1, 1, 1, 2, 3]))
+print(judge1([1, 1, 2, 3, 4]))
+print(judge1([1, 2, 3, 4, 5]))
+
+
+
+#---3---
+def omikuji():
+    l = dice5()
+    result = judge1(l)
+    if result == '':
+        result = judge0(l)
+    return l, result
+
+print(omikuji())
